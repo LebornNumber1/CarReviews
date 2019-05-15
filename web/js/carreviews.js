1cf05304efd73104bdcb2a73cf3abe7aaf1ce49f
吴悠 <37774625+LebornNumber1@users.noreply.github.com>
2019-05-15 16:31:31 +0800

    添加了一些页面的js文件
    对于打灰太狼的js文件提交，对于主页面的轮播图js文件提交等等

--- web/js/carreviews.js
+++ web/js/carreviews.js
@@ -3,29 +3,72 @@
  */
 
 window.onload = function () {
-    var img = document.getElementById("img");
-    var pre = document.getElementById("pre");
-    var next = document.getElementById("next");
+    var lis = $("ul").children;
+    var currentIndex = 0,indicatorIndex = 0;
 
-    var maxIndex = 6,minIndex = 1,currentIndex = 1;
-    pre.onclick = function () {
-         //alert("点击了上一张");
-        if(currentIndex === minIndex){
-            currentIndex = maxIndex;
-        }
-        else{
-            currentIndex--;
-        }
-        img.setAttribute("src","../image/qiehuan"+currentIndex+".jpg");
+    //在后面克隆一个第一张图片
+    $("ul").appendChild(lis[0].cloneNode(true));
+
+    //动态创建ol中内容
+    for(var i =0 ;i < lis.length - 1;i++){
+        $("ol").appendChild(document.createElement("li"));
+    }
+
+    $("ol").children[0].className = "current";
+
+    var olLis = $("ol").children;
+    //鼠标进入圆点
+    for(var j =0 ; j< olLis.length;j++){
+        (function (j) {
+            var li = olLis[j];
+            li.onmouseover = function () {
+                if(li.className !== "current"){
+                    for(var i=0;i<olLis.length;i++){
+                        olLis[i].className = "";
+                    }
+                    this.className = "current";
+
+                    //让ul动起来显示到对应图片
+                    constant($("ul"),-(1080 * j),1000);
+                    currentIndex = indicatorIndex = j;
+                }
+            };
+        })(j);
+    }
+
+    //6.自动轮播
+    var timer;
+    clearInterval(timer);
+    timer= setInterval(autoplay, 2000);
+
+    //7.清除和设置定时器
+    $("content_img").onmouseover = function () {
+        clearInterval(timer);
     };
-    next.onclick = function () {
-         //alert("点击了下一张");
-        if(currentIndex === maxIndex){
-            currentIndex = minIndex;
-        }
-        else{
-            currentIndex++;
-        }
-        img.setAttribute("src","../image/qiehuan"+currentIndex+".jpg");
+
+    $("content_img").onmouseout = function () {
+        timer = setInterval(autoplay, 2000)
     };
+
+
+    function autoplay() {
+        //6.1  ul滚起来
+        currentIndex++;
+        if (currentIndex > lis.length - 1) {
+            $("ul").style.left = 0;
+            currentIndex = 1;
+        }
+        constant($("ul"), -currentIndex * 1080, 30);
+
+        //6.2 指示器滚起来
+        indicatorIndex++;
+        if (indicatorIndex > olLis.length - 1) {
+            indicatorIndex = 0;
+        }
+        for (var i = 0; i < olLis.length; i++) {
+            olLis[i].className = "";
+        }
+
+        olLis[indicatorIndex].className = "current";
+    }
 };
